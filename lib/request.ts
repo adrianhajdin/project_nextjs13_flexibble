0610916343459d65799f078ea133a21c3b083c62
adrianhajdin <adrianhajdin00@gmail.com>
2023-06-20 18:56:56 +0200

    ...

--- lib/request.ts
+++ lib/request.ts
@@ -9,6 +9,7 @@ export const makeRequest = async (url: string, options: RequestOptions) => {
     const isProduction = process.env.NODE_ENV === 'production';
     const fullUrl = isProduction ? `${process.env.NEXT_PUBLIC_SERVER_URL}/${url}` || '' : `http://localhost:3000/${url}`;
 
+    console.log({isProduction, fullUrl})
     try {
         const response = await fetch(fullUrl, {
             method,

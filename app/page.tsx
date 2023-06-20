749a47f0ec0ee146c37103060ff9131b7f048825
adrianhajdin <adrianhajdin00@gmail.com>
2023-06-20 13:15:02 +0200

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -22,20 +22,19 @@ const Home = async ({ searchParams }: Props) => {
   const { apiUrl, apiKey } = await getApiConfig();
   const isProduction = process.env.NODE_ENV === 'production';
   const baseUrl = isProduction ? `${process.env.SERVER_URL || ''}` : `http://localhost:3000/`;
-  // const response = await fetch(`${baseUrl}/api/posts?category=${category}&cursor=${cursor}`);
+  const response = await fetch(`${baseUrl}/api/posts?category=${category}&cursor=${cursor}`);
+
+  // const response = await fetch(apiUrl, {
+  //   headers: { 'x-api-key': apiKey },
+  //   method: 'POST',
+  //   body: JSON.stringify({ query: getProjectsQueryNew({ category, cursor }) }),
+  //   next: { tags: [getProjectsQueryNew({ category, cursor })] }
+  // })
+
+  // const { data } = await response.json()
 
 
-  const response = await fetch(apiUrl, {
-    headers: { 'x-api-key': apiKey },
-    method: 'POST',
-    body: JSON.stringify({ query: getProjectsQueryNew({ category, cursor }) }),
-    next: { tags: [getProjectsQueryNew({ category, cursor })] }
-  })
-
-  const { data } = await response.json()
-
-
-  console.log({data})
+  // console.log({data})
 
   // const client = new GraphQLClient(apiUrl, {
   //   headers: {
@@ -46,7 +45,7 @@ const Home = async ({ searchParams }: Props) => {
   // const mutation = getProjectsQueryNew({ category, cursor });
   // const data = await client.request(mutation);
 
-  // const projects = await response.json();
+  const data = await response.json();
 
   const projectsToDisplay = data?.projectCollection?.edges || [];
 

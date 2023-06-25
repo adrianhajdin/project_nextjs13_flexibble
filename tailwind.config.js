1eda2fa740f7a11f7a0ce800a0581c7a1f4ef76d
manish <mnishofficial@gmail.com>
2023-06-25 23:19:41 +0530

    comma added

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         'light-white-300': '#F3F3F4',
         'light-white-400': '#E2E5F1',
         'light-white-500': '#E4E4E4',
-        gray: '#4D4A4A',
+        'gray': '#4D4A4A',
         'gray-100': '#3d3d4e',
         'black-100': '#252525',
         'primary-purple': '#9747FF',
